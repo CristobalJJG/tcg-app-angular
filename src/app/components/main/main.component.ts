b0654b7ca37802e596781869d242b3a1e73910aa
CristóbalJJG <cristobal_jjg@hotmail.com>
2024-02-09 12:19:07 +0000

    Construcción real del main con el deck

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Card } from 'pokemon-tcg-sdk-typescript/dist/sdk';
+import { PokemonTcgService } from 'src/app/services/pokemon-tcg.service';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  deck: Card[] = [];
+  constructor(private pk: PokemonTcgService) {
+    this.getPokemon();
+  }
 
+  async getPokemon() {
+    await this.pk.getPokemonBySet().then((data: Card[]) => {
+      data.forEach((card: Card) => { this.deck.push(card); });
+    });
+  }
 }
