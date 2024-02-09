ac0f705ed3a39016ac5dace9738a562cdb9eb870
CristóbalJJG <cristobal_jjg@hotmail.com>
2024-02-09 12:19:20 +0000

    Maquetación de las cartas

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,23 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Card } from 'pokemon-tcg-sdk-typescript/dist/sdk';
-import { PokemonTcgService } from 'src/app/services/pokemon-tcg.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
-  card: Card[] = [];
-  constructor(private pk: PokemonTcgService) { }
+export class CardComponent {
+  @Input() card!: Card | any;
 
-  ngOnInit() {
-    console.log(this.card.length);
-    this.getPokemon();
-    console.log(this.card.length);
-  }
-  async getPokemon() {
-    //await this.pk.getPokemonById("xy1-1").then((data: Card) => this.card.push(data));
-    await this.pk.getPokemonBySet("name:Gengar").then((data: Card[]) => this.card.push(data[0]));
-  }
 }
